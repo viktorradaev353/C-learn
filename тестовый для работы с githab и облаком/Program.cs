ea46b6f1fcdd99da540dfb6b5139030b8de05ae7
Виктор Радаев <Piroliz.nt@yandex.ru>
2023-04-26 17:47:34 +0500

    тест

---
+++ тестовый для работы с githab и облаком/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace тестовый_для_работы_с_githab_и_облаком
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
